7215ec16fe2de3d3fdc88a436b8bc05fb4af36d2
harry14 <chizoharsh@gmail.com>
2018-02-25 17:30:01 +0530

    Added connecting insert page

--- phd_info_mgmt_system/views.py
+++ phd_info_mgmt_system/views.py
@@ -1,6 +1,6 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from phd_info_management.models import PhDScholar, PhDCourses, PhDThesis, \
+from phd_info_mgmt_system.models import PhDScholar, PhDCourses, PhDThesis, \
                                        PhDScholarCourses
 
 # Create your views here.
@@ -14,6 +14,11 @@ def phd_update_page(request):
   return render_to_response('phd_info_mgmt_system/phd_update.html',
                             {}, context)
 
+def phd_insert_page(request):
+  context = RequestContext(request)
+  return render_to_response('phd_info_mgmt_system/phd_insert.html',
+                            {}, context)  
+
 def phd_course_insert(request):
   context = RequestContext(request)
   if request.method == 'POST':
@@ -22,7 +27,7 @@ def phd_course_insert(request):
       if key != 'csrfmiddlewaretoken':
         request_dict[key] = request.POST.get(key)
     phd_course = PhDCourses(**request_dict)
-    course_id = request.POST.get('course_id')
+    course_id = request_dict.get('course_id')
     records = PhDCourses.objects.filter(course_id=course_id)
     if len(records) > 0:
       error_msg = 'The Course with ID {id} already' \
@@ -30,7 +35,8 @@ def phd_course_insert(request):
       return render_to_response('phd_info_mgmt_system/phd_course_form.html',
                                 {'course_exists': error_msg}, context)
     phd_course.save()
-    return render_to_response('phd_info_mgmt_system/phd_update.html', {}, context)
+    success_msg = 'Course successfully added!'
+    return render_to_response('phd_info_mgmt_system/phd_course_form.html', {'course_exists': success_msg}, context)
   else:
     return render_to_response('phd_info_mgmt_system/phd_course_form.html', {}, context)
 
@@ -52,7 +58,8 @@ def phd_scholar_insert(request):
                                  'form_origin': '/phd/phd_scholar_insert/'},
                                 context)
     phd_scholar.save()
-    return render_to_response('phd_info_mgmt_system/phd_update.html', {}, context)
+    success_msg = 'Scholar successfully added!'
+    return render_to_response('phd_info_mgmt_system/phd_scholar_form.html', {'id_number_exists': success_msg}, context)
   else:
     return render_to_response('phd_info_mgmt_system/phd_scholar_form.html',
                               {'form_origin': '/phd/phd_scholar_insert/'}, context)
